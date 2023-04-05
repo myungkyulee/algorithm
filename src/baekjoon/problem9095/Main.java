bd3ba5e4165ba1a24b0e6ef7ef02309d8552597d
Myungkyulee <skdlaudrb@naver.com>
2023-04-05 23:13:21 +0900

    백준 9095 문제풀이 코드 추가

---
+++ src/baekjoon/problem9095/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.problem9095;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[12];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            bw.write(dp[n] + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
